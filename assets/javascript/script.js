3b88e21f1dc852ce4f7cbc748ab9b1d300f0c514
asmaahamid02 <asmaahamid002@gmail.com>
2022-09-16 17:49:20 +0300

    coment out previous code and declare constants needed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,71 @@
-const cards = document.querySelectorAll('.card')
+// const cards = document.querySelectorAll('.card')
 
-cards.forEach((card) =>
-  card.addEventListener('click', () => {
-    card.classList.toggle('flip')
-  })
-)
+// let cardFlipped = false
+// let card_one, card_two
+
+// const flipCard = (element) => () => {
+//   element.classList.toggle('flip')
+
+//   if (!cardFlipped) {
+//     cardFlipped = true
+//     card_one = element
+//   } else {
+//     cardFlipped = false
+//     card_two = element
+//   }
+
+//   if (card_one && card_two) {
+//     if (card_one.dataset.language === card_two.dataset.language) {
+//       card_one.removeEventListener('click', flipCard)
+//       card_two.removeEventListener('click', flipCard)
+//     }
+//   }
+
+//   console.log(cardFlipped, card_one, card_two)
+// }
+// cards.forEach((card) => card.addEventListener('click', flipCard(card)))
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cardsList = [
+    {
+      name: 'html',
+      src: './assets/images/html.png',
+    },
+    {
+      name: 'html',
+      src: './assets/images/html.png',
+    },
+    {
+      name: 'js',
+      src: './assets/images/js.png',
+    },
+    {
+      name: 'js',
+      src: './assets/images/js.png',
+    },
+    {
+      name: 'python',
+      src: './assets/images/python.png',
+    },
+    {
+      name: 'python',
+      src: './assets/images/python.png',
+    },
+  ]
+
+  cardsList.sort(() => 0.5 - Math.random())
+
+  const cards_container = document.querySelector('.cards-container')
+
+  const flips_num = document.querySelector('.flips-num')
+  const found_num = document.querySelector('.found-num')
+
+  const clickedCards = []
+  const cards_ids = []
+  const total_cards = 6
+  const flips_counter = 0
+  const found_counter = 0
+
+  flips_num.textContent = flips_counter
+  found_num.textContent = found_counter
+})
